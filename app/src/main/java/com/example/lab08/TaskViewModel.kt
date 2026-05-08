f2bf0bd598de79b49bf658cf5a824a5b9a58a3fe
katherinequisper-glitch <katherine.quispe.r@tecsup.edu.pe>
2026-05-08 17:15:07 -0500

    feat: add TaskViewModel

---
+++ app/src/main/java/com/example/lab08/TaskViewModel.kt
@@ -0,0 +1,42 @@
+package com.example.lab08
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+
+class TaskViewModel(private val dao: TaskDao) : ViewModel() {
+
+    private val _tasks = MutableStateFlow<List<Task>>(emptyList())
+    val tasks: StateFlow<List<Task>> = _tasks
+
+    init {
+        viewModelScope.launch {
+            _tasks.value = dao.getAllTasks()
+        }
+    }
+
+    fun addTask(description: String) {
+        val newTask = Task(description = description)
+        viewModelScope.launch {
+            dao.insertTask(newTask)
+            _tasks.value = dao.getAllTasks()
+        }
+    }
+
+    fun toggleTaskCompletion(task: Task) {
+        viewModelScope.launch {
+            val updatedTask = task.copy(isCompleted = !task.isCompleted)
+            dao.updateTask(updatedTask)
+            _tasks.value = dao.getAllTasks()
+        }
+    }
+
+    fun deleteAllTasks() {
+        viewModelScope.launch {
+            dao.deleteAllTasks()
+            _tasks.value = emptyList()
+        }
+    }
+}
